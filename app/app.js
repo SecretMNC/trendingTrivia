2fccc0f66a638d0dca91aec5fbe43b959083b1af
Stefano Martinengo <stefanomartinengo3@gmail.com>
2017-11-17 10:33:37 -0700

    added routes with appjs

---
+++ app/app.js
@@ -0,0 +1,18 @@
+angular.module('triviaTrends', [ui.router]).config( function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+    .state('home', {
+        url:'/',
+        templateUrl: 'app/'
+    }).state('questionlist', {
+        url:'/questionlist',
+        templateUrl:'app/'
+    }).state('questioneditor', {
+        url:'/questioneditor',
+        templateUrl:'app/'
+    })
+
+    $urlRouterProvider
+    .otherwise('/')
+
+})
